859718534bc6801a5038b1a82da0b9f9d67f7ef1
PRIYANSHU NEGI <priyanshunegi752@gmail.com>
2023-08-30 23:00:37 +0530

    Update2

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from "react";
+import { Provider } from "react-redux"
 import ReactDOM from "react-dom";
+import store from "./store/ReduxStore";
 import App from "./App";
 
 ReactDOM.render(
-  <App/>,
+  <Provider store ={store}>
+
+  <App/>
+  </Provider>,
   document.getElementById("root")
 );
+// 1.02
